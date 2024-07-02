1359357de69eb73c1dbf8ab55bf338dc42f0f989
kang-theo <theo.cy.kang@gmail.com>
2024-07-02 16:06:19 +0800

    feat: pie chart with NuGet liveCharts_wpf supported

---
+++ device-dashboard/Models/AlarmItemModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace device_dashboard.Models
+{
+    public class AlarmItemModel
+    {
+        public int Index { get; set; }
+        public string Time { get; set; }
+        public string Message { get; set; }
+        public string Status { get; set; }
+    }
+}
--- device-dashboard/ViewModels/MainViewModel.cs
+++ device-dashboard/ViewModels/MainViewModel.cs
@@ -13,12 +13,21 @@ namespace device_dashboard.ViewModels
         public List<DeviceItemModel> DeviceList { get; set; } =
             new List<DeviceItemModel>();
 
+        public List<AlarmItemModel> AlarmList { get; set; } =
+            new List<AlarmItemModel>();
+
         public MainViewModel()
         {
             DeviceList.Add(new DeviceItemModel() { Index = 1 });
             DeviceList.Add(new DeviceItemModel() { Index = 2 });
             DeviceList.Add(new DeviceItemModel() { Index = 3 });
             DeviceList.Add(new DeviceItemModel() { Index = 4 });
+
+            for(int i = 0; i < 15; i++)
+            {
+                AlarmList.Add(new AlarmItemModel() { Index = i + 1 });
+            }
         }
+
     }
 }
